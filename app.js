9732647105e7b65cb391e18acf5d9382d4ccbb71
Josh <leong.jo@gmail.com>
2017-10-11 20:15:32 -0600

    added dir db w knexfile

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
+var todo = require('./routes/todo');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', routes);
+app.use('/todo', todo);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
